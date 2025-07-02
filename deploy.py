09e5121267ff2ffb1a3a83d3a04b8fe35fcfd74e
Lamel <lamel466@gmail.com>
2025-07-02 18:31:39 +0200

    deploy

--- deploy.py
+++
@@ -1,7 +0,0 @@
-from etl_pipeline.etl_flow import main 
-
-if __name__ == "__main__":
-    main.deploy(
-        name="etl-deployment",
-        work_pool_name="etl-workpool",  # Replace this
-    )
